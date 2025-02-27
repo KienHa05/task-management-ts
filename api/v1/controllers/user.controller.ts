f9c2e3c04546a6fbbaf9616e67b57dc89ae42040
KienHa05 <kienhgph50150@gmail.com>
2025-02-27 12:15:17 +0700

    11. Tính Năng Đăng Ký
    BUỔI 52

---
+++ api/v1/controllers/user.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import md5 from "md5";
+import { generateRandomString } from "../../../helpers/generate";
+import User from "../models/user.model";
+
+// [POST] /api/v1/users/register
+export const register = async (req: Request, res: Response) => {
+    const existEmail = await User.findOne({
+        email: req.body.email,
+        deleted: false
+    });
+
+    if (existEmail) {
+        res.json({
+            code: 400,
+            message: "Email Đã Tồn Tại!"
+        });
+    } else {
+        req.body.password = md5(req.body.password);
+
+        req.body.token = generateRandomString(30);
+
+        const user = new User(req.body);
+        const data = await user.save();
+
+        const token = data.token;
+
+        res.json({
+            code: 200,
+            message: "Tạo Tài Khoản Thành Công!",
+            token: token
+        });
+    }
+};
